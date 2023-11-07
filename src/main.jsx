935b1ebe1ef881ed6b49fa65ce6b47046bf08ec5
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-11-07 14:14:58 +0600

    added framer motion

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import MainRoutes from "./Routes/MainRoutes.jsx";
 import AuthProviders from "./Providers/AuthProviders.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
+import { motion } from "framer-motion";
+import { scroll } from "framer-motion/dom"; // Import the scroll function
 
 const queryClient = new QueryClient();
 
@@ -14,9 +16,25 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthProviders>
       <QueryClientProvider client={queryClient}>
         <HelmetProvider>
-          <RouterProvider router={MainRoutes}></RouterProvider>
+          <RouterProvider router={MainRoutes}>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              Your content here
+            </motion.div>
+          </RouterProvider>
         </HelmetProvider>
       </QueryClientProvider>
     </AuthProviders>
   </React.StrictMode>
 );
+
+// Select the progress wheel element
+const progressWheel = document.querySelector(".progress");
+
+// Add the scroll animation
+scroll((progress) => {
+  progressWheel.style.strokeDasharray = `${progress}, 1`;
+});
